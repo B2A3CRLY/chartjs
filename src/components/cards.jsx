8df021a422edd0b07ef8bdbe997aa15bdd98cf1a
Babacar Ly <babacarly111@gmail.com>
2019-08-07 17:45:03 +0000

    third commit

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import Card from '../components/cardUI';
-import img1 from '../assets/team2.jpeg';
+import img7 from '../assets/team7.jpeg';
+import img4 from '../assets/team4.jpeg';
+import img1 from '../assets/team1.jpeg';
 export default class Cards extends Component {
     render() {
         return (
             <div className="container-fluid d-flex justify-content-center">
                 <div className="row">
-                    <div className="col-md-4 rot">
+                    <div className="col-md-4">
                         <Card imgsrc={img1} title="The chosen one"/>
                     </div>
                     <div className="col-md-4">
-                        <Card imgsrc={img1} title="Babacar Ly"/>
+                        <Card imgsrc={img7} title="The family"/>
                     </div>
                     <div className="col-md-4 rot180">
-                    <Card imgsrc={img1} title="The way of success"/>
+                    <Card imgsrc={img4} title="The way of success"/>
                     </div>
                 </div>
             </div>
